84e81b36ecaa7460926f700c68d8c03014cb08a6
Andrew Nolte <anolte512@gmail.com>
2019-04-04 14:39:52 -0500

    added timestamp, trigger, and fixed consistency issue

--- StoreMessageService.js
+++ StoreMessageService.js
@@ -3,33 +3,28 @@
 //also a Collection called cpu has to be made
 
 function StoreMessageService(req, resp) {
-  ClearBlade.init({
-    request: req
-  });
+    ClearBlade.init({
+        request: req
+    });
 
-  var msg = ClearBlade.Messaging();
-  var unixTimeNano = new Date().getTime();
-  var unixTimeMilli = unixTimeNano / 1000;
+    var msg = ClearBlade.Messaging();
+    var unixTimeNano = new Date().getTime();
+    var unixTimeMilli = unixTimeNano / 1000;
 
-  msg.getMessageHistoryWithTimeFrame("cpuTopic", 1, unixTimeMilli, null, null, function (err, body) {
-    if (err) {
-      resp.error("message history error : " + JSON.stringify(body));
-    } else {
-      var col = ClearBlade.Collection({
+    var colec = ClearBlade.Collection({
         collectionName: "cpu"
-      });
-      var pct = parseFloat(body[0].message);
-      col.create({
-        percent: pct
-      }, function (err, data) {
-        if (err) {
-          resp.error("creation error : " + JSON.stringify(data));
-        } else {
-          resp.success(data);
-        }
-      });
-    }
-  });
+    });
+    var blob = JSON.parse(req.params.body);
 
-  resp.success("Success");
+    colec.create({
+        percent: (Math.round(blob.pct * 10) / 10),
+        timestamp: blob.timestamp
+    }, function (err, data) {
+        if (err) {
+            resp.error("creation error : " + JSON.stringify(data));
+        } else {
+            resp.success(data);
+        }
+    });
+    resp.success("Success");
 }
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from clearblade.ClearBladeCore import System, Query
 import psutil  # for getting system info
 import time
+import json
 
 SystemKey = "f2a493ca0b82d7998df8f4f9ad47"
 SystemSecret = "F2A493CA0B94ECA9ADCD9ABAF98801"
@@ -12,16 +13,28 @@ bobby = mySystem.User("rickybobby@gmail.com", "iynfyl")
 mqtt = mySystem.Messaging(bobby)
 code = mySystem.Service("StoreMessageService")
 
+# for x in range(10):
+#     print(psutil.cpu_percent())
+#     time.sleep(.5)
+#     # code.execute(bobby, {})
+#     time.sleep(.5)
+
 
 def on_connect(client, userdata, flags, rc):
-    for x in range(25):
-        client.publish("cpuTopic", psutil.cpu_percent())
-        time.sleep(.5)
-        code.execute(bobby, {})
-        time.sleep(.5)
+    for x in range(10):
+        pct = psutil.cpu_percent()
+        timeStamp = time.time()
+        blob = {
+            "pct":
+            pct,
+            "timestamp": str(timeStamp)
+        }
+        print("publishing blob: " + json.dumps(blob))
+        client.publish("cpuTopic", json.dumps(blob))
+        time.sleep(1)
 
 
 mqtt.on_connect = on_connect
 mqtt.connect()
-time.sleep(30)
+time.sleep(12)
 mqtt.disconnect()
